c193a4d1d56150334341d18e13f89e83c817bdec
Iveta Hristova <iveta.hristova.hristovaa@gmail.com>
2019-04-28 17:22:36 +0300

    refactoring - models

--- marvel/models.py
+++ marvel/models.py
@@ -5,15 +5,15 @@ from django.db import models
 class Character(models.Model):
     def get_upload_to(self, filename):
         return 'character_images/%s' % ( filename)
-    hero = models.CharField(max_length=128, null=True)
-    first_name = models.CharField(max_length=128,null=True, blank=True)
-    last_name = models.CharField(max_length=128, null=True, blank=True)
+    hero = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=128, blank=True)
+    last_name = models.CharField(max_length=128, blank=True)
     height = models.DecimalField(max_digits=5, decimal_places=1,null=True, blank=True)
-    weight = models.DecimalField(max_digits=5, decimal_places=1, null=True, blank=True)
+    weight = models.DecimalField(max_digits=5, decimal_places=1, blank=True, null=True)
     gender = models.CharField(max_length=10)
-    biography =  models.TextField(null=True, blank=True)
-    eyes = models.CharField(max_length=128,null=True, blank=True)
-    hair =  models.CharField(max_length=128,null=True, blank=True)
+    biography =  models.TextField(blank=True)
+    eyes = models.CharField(max_length=128, blank=True)
+    hair =  models.CharField(max_length=128, blank=True)
     profile_picture =  models.ImageField(upload_to=get_upload_to, null=True, blank=True)
 
     def __str__(self):
@@ -24,7 +24,7 @@ class Group(models.Model):
         return 'group_images/%s' % ( filename)
     name  = models.CharField(max_length=128)
     profile_picture =  models.ImageField(upload_to=get_upload_to, null=True, blank=True)
-    history =  models.TextField(null=True, blank=True)
+    history =  models.TextField(blank=True)
     members =  models.ManyToManyField(Character, blank=True)
     def __str__(self):
         return self.name 
